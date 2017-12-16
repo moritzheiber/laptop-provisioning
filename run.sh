8b151a1cc55e0bb8f0ea090404f9318b4733238d
Moritz Heiber <mheiber@thoughtworks.com>
2017-12-16 23:05:48 +0100

    Added ability to specify moar arguments

--- run.sh
+++ run.sh
@@ -2,11 +2,12 @@
 
 set -xue
 
+EXTRA_ARGUMENTS="${1}"
 EXTRA_VARS="${PROVISIONING_EXTRA_VARS:-}"
 
 ansible_run_playbook() {
   echo "Running Ansible"
-  ansible-playbook ansible/playbook.yml --ask-become-pass -v --connection=local --extra-vars "${EXTRA_VARS}"
+  ansible-playbook ansible/playbook.yml --ask-become-pass -v --connection=local --extra-vars "${EXTRA_VARS}" "${EXTRA_ARGUMENTS}"
 }
 
 ansible_run_playbook
