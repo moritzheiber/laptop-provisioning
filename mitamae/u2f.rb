2435d5c142240a6bfcad9c2c8ef26144cc4718af
Moritz Heiber <hello@heiber.im>
2018-11-17 19:13:56 +0100

    Add corresponding udev rules for Nitrokey

--- mitamae/u2f.rb
+++ mitamae/u2f.rb
@@ -1,3 +1,9 @@
+remote_file '/etc/udev/rules.d/41-nitrokey.rules' do
+  source 'files/41-nitrokey.rules'
+  mode '0644'
+  notifies :run, 'execute[service udev restart]', :immediately
+end
+
 remote_file '/usr/share/pam-configs/u2f' do
   source 'files/u2f'
   mode '0644'
@@ -5,5 +11,9 @@ remote_file '/usr/share/pam-configs/u2f' do
 end
 
 execute 'pam-auth-update --enable u2f' do
+  action :nothing
+end
+
+execute 'service udev restart' do
   action :nothing
 end
