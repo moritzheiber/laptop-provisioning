ffa91b550d1748ab70df60f9334b726239811aa1
Moritz Heiber <hello@heiber.im>
2018-11-17 18:46:17 +0100

    Only run pam-auth-update on updates, require U2F auth

--- mitamae/u2f.rb
+++ mitamae/u2f.rb
@@ -1,6 +1,9 @@
 remote_file '/usr/share/pam-configs/u2f' do
   source 'files/u2f'
   mode '0644'
+  notifies :run, 'execute[pam-auth-update --enable u2f]'
 end
 
-execute 'pam-auth-update --enable u2f'
+execute 'pam-auth-update --enable u2f' do
+  action :nothing
+end
